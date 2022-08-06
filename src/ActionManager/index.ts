24fc5d590f638a1cc5c78331f6040861296c593e
linguanghui <guanghui.lin@lixinchuxing.com>
2022-08-06 11:45:08 +0800

    feat: ActionManager交互管理器初始化

---
+++ src/ActionManager/index.ts
@@ -0,0 +1,35 @@
+/**
+ * author: huihui
+ * 
+ */
+class ActionMnager {
+    page: string;
+    
+    map = new Map();
+
+    pageInstance: Map<string, object>;
+
+    use (page: string) {
+        if (this.map.has(page)) {
+            this.pageInstance = this.map.get(this.page);
+        }
+        else {
+            this.pageInstance = new Map();
+        }
+    }
+
+    private enQueue () {
+
+    }
+}
+
+
+class Action {
+
+}
+
+let Actionman = new ActionMnager();
+Actionman.page = '123'
+
+
+export default new ActionMnager();
