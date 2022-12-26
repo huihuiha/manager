50257a9b56df02286a6731a0ca38b23403efc1bd
linguanghui <1476465526@qq.com>
2022-12-26 21:21:03 +0800

    chore: 项目构建

---
+++ packages/index.ts
@@ -0,0 +1,1 @@
+export { default as ActionMnager } from './ActionManager';
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
 module.exports = {
   input: 'packages/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
-    plugins: [terser()],
-  },
+  output: [
+    {
+      format: 'cjs',
+      file: 'lib/manager.cjs.js',
+    },
+    {
+      format: 'es',
+      file: 'lib/manager.esm.js',
+    },
+  ],
+  plugins: [typescript(), terser()],
 };
